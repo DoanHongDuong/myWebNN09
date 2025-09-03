1e094ad73455c0f9736ec369838d17cedcbbb376
DoanHongDuong <120620120+DoanHongDuong@users.noreply.github.com>
2025-09-03 17:30:33 +0000

    yupdate

--- WebApp1/app/Http/Controllers/Controller.php
+++ WebApp1/app/Http/Controllers/Controller.php
@@ -2,7 +2,24 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;  // Nếu cần phải sử dụng Request, nếu không có thì có thể bỏ dòng này.
+
+class TestController extends Controller  // Cái "Controller" phải viết hoa
 {
-    //
+    public function index()
+    {
+        $duong = "Duong hehe";  // Chuỗi cần hiển thị
+        $test = true;            // Biến test
+
+        echo $duong;             // In ra $duong
+
+        $tuoi = 20;              // Khai báo và gán giá trị cho biến $tuoi
+
+        if ($tuoi > 18) {
+            echo "Du dieu kien bau cu";  // Điều kiện nếu tuổi > 18
+        } else {
+            echo "Chua du dieu kien bau cu";  // Điều kiện nếu tuổi <= 18
+        }
+    }
 }
+
